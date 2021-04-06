922a7ddaae65c2c7090106d1adb93ff90aa5bac2
capella220 <72174515+capella220@users.noreply.github.com>
2021-04-06 17:28:54 -0400

    Update index.php

--- index.php
+++ index.php
@@ -1,24 +1,17 @@
 <html>
-<?php
-include "./Navbar/navbar.php"
-?>
 <head>
 	<title> 
 		University Faculty on Committees
 	</title>
 	<link href="style.css" rel="stylesheet">
 <body>
-	<div id="navbar">
-		<nav>
-		  <ul>
-		   <li><a href="index.pho">Home</a></li>
-		   <li><a href="voting.php">Voting</a></li>
-		   <li><a href="reports.php">Reports</a></li>
-		  </ul>
-		 </nav>
-	 </div id="navbar">
-	 
-	 
+<nav>
+  <ul>
+   <li><a href="index.php">Home</a></li>
+   <li><a href="voting.php">Voting</a></li>
+   <li><a href="report.php">Reports</a></li>
+  </ul>
+ </nav>
 	 
 </head> 
 </body>
