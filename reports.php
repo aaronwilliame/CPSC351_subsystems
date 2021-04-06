ecc6daf218e066028a63b195cba1012fe442bc21
capella220 <72174515+capella220@users.noreply.github.com>
2021-04-06 17:28:52 -0400

    Update reports.php

--- reports.php
+++ reports.php
@@ -6,9 +6,14 @@
 	</title>
 	<link href="style.css" rel="stylesheet">
 <body>
-
-<main>
-	
+<nav>
+  <ul>
+   <li><a href="index.php">Home</a></li>
+   <li><a href="voting.php">Voting</a></li>
+   <li><a href="report.php">Reports</a></li>
+  </ul>
+ </nav>
+	 
 	
 </head> 
 </body>
